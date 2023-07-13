02db9da55ea9dac8b319f643f99f9de2a5c3e86b
Arjun <arjundasback515@gmail.com>
2023-07-13 22:35:30 +0600

    navigation bar design done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,9 @@ import {createBrowserRouter} from "react-router-dom";
 import Main from "../LayOut/Main";
 import Shop from "../page/Shop/Shop";
 import Home from "../page/Home/Home";
+import Blog from "../page/Blog/Blog";
+import About from "../page/About/About";
+import Contact from "../page/Contact/Contact";
 
   export const router = createBrowserRouter([
     {
@@ -19,6 +22,18 @@ import Home from "../page/Home/Home";
         {
             path:"/shop",
             element: <Shop></Shop>
+        },
+        {
+            path:"/blog",
+            element: <Blog></Blog>
+        },
+        {
+            path:"/about",
+            element: <About></About>
+        },
+        {
+            path:"/contact",
+            element: <Contact></Contact>
         },
 
       ]
--- src/page/Header/Header.jsx
+++ src/page/Header/Header.jsx
@@ -1,5 +1,7 @@
-import {  FaPhoneAlt } from 'react-icons/fa';
+import {  FaPhoneAlt,FaProductHunt,FaCartPlus } from 'react-icons/fa';
 import { AiOutlineMail} from 'react-icons/ai';
+import { BiSearch} from 'react-icons/bi';
+import { MdAccountCircle} from 'react-icons/md';
 import logo from "../../assets/images/sidebar/LogoMakr-3KGA0z.png"
 import { Link } from 'react-router-dom';
 const Header = () => {
@@ -24,18 +26,31 @@ const Header = () => {
                 </div>
             </header>
 
-            <header className=" p-2 ">
+            <header className=" header-nav p-1 ">
                 <div className=" container-xxl ">
-                    <div className="row ">
+                    <div className="row d-flex align-items-center justify-content-between">
                         <div className="col-2">
                             <Link to={"/"}><img src={logo} alt=""  className='img-fluid logo'/></Link>
                         </div>
-                        <div className="col-5">
-                            <Link to={"/"}>HOME</Link>
-                            <Link to={"/shop"}>SHOP</Link>
-                            <Link to={"/blog"}>BLOG</Link>
-                            <Link to={"/about"}>ABOUT</Link>
-                            <Link to={"/contact"}>CONTACT</Link>
+                        <div className="col-3 d-flex align-items-center mt-3">
+                            <div className="input-group mb-3">
+                            <span className="input-group-text" id="basic-addon1"><BiSearch /></span>
+                            <input type="text" className="form-control" placeholder="find products" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                            <button className="input-group-text" id="basic-addon2">search</button>
+                            </div>
+                        </div>
+                        <div className="col-4 d-flex align-items-center justify-content-between ">
+                            <Link className='text-decoration-none' to={"/"}>HOME</Link>
+                            <Link className='text-decoration-none' to={"/shop"}>SHOP</Link>
+                            <Link className='text-decoration-none' to={"/blog"}>BLOG</Link>
+                            <Link className='text-decoration-none' to={"/about"}>ABOUT</Link>
+                            <Link className='text-decoration-none' to={"/contact"}>CONTACT</Link>
+                        </div>
+                        <div className="col-3 d-flex align-items-center justify-content-center  ">
+                            <Link className='text-decoration-none p-2' to={"/"}><FaProductHunt className='fs-2' /></Link>
+                            <Link className='text-decoration-none p-2' to={"/shop"}><MdAccountCircle className='fs-1'/></Link>
+                            <Link className='text-decoration-none p-2' to={"/blog"}><FaCartPlus className='fs-2' /></Link>
+                            
                         </div>
                     </div>
                 </div>
